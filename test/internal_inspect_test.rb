b8113d1f0820b522f0c635e42959f23dbdcbd521
yui-knk <spiketeika@gmail.com>
2017-07-17 00:26:07 +0900

    Remove a meaningless test case

--- test/internal_inspect_test.rb
+++ test/internal_inspect_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class InternalInspectTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::InternalInspect::VERSION
-  end
-
   def test_static_sym_p
     assert "a".to_sym.static_sym?
     assert !"あ".to_sym.static_sym?
